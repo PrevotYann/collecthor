92fc1d33050a3eaeda4f8541b8b5d4e680523390
Prévot Yann <48239947+PrevotYann@users.noreply.github.com>
2024-05-06 15:11:38 +0200

    Update Home.js
    Home calls api hello to preload it

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function Home() {
+  useEffect(() => {
+    const url = `${process.env.REACT_APP_API_URL}/hello`;
+    try {
+      const response = await axios.get(url);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  }, []);
+  
   return <div>Click on cards.</div>;
 }
 
